043b44c4e59c624cfea07395dd7de87cd31f277f
Rafael Victor <rafavictorgs@gmail.com>
2022-03-18 12:59:15 -0300

    feat: Criando o componente Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderStyle from './styles';
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      <h1>
+        Olá
+      </h1>
+    </HeaderStyle>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const HeaderStyle = styled.header`
+  background-color: #5c0000;
+  height: 12rem;
+`;
+
+export default HeaderStyle;
